f4a675add480ec2ce882b151e9935038e90625b1
yeyecorrea <yeyecorrea838@gmail.com>
2022-04-17 16:28:44 -0500

    the multiplication table is finished

---
+++ ATM/MultiplicationTables/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] tables = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+
+for (int i = 1; i < tables.Length; i++)
+{
+    Console.WriteLine($"\n----------Tabla: {i}----------\n");
+    for (int j = 1; j <= 12; j++)
+    {
+        int result = i * j;
+        Console.WriteLine($"{i} X {j} = {result}");
+    }
+}
