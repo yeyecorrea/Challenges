81ba39e453b9c10e08a42e3419b77ff9c4291a25
yeyecorrea <yeyecorrea838@gmail.com>
2022-04-17 13:54:08 -0500

    the bank menu is finished

---
+++ ATM/ATM/Program.cs
@@ -0,0 +1,35 @@
+﻿int options = 0;
+while (options != 4)
+{
+    Console.WriteLine(@"Bienvenido al cajero, que deseas hacer hoy:
+       1 - Ver saldo
+       2 - Retirar
+       3 - Depositar
+       4 - Salir
+    ");
+    options = Convert.ToInt32(Console.ReadLine());
+    Console.Clear();
+    switch (options)
+    {
+        case 1:
+            Console.WriteLine($@"Saldo
+             - Tu saldo es de {0:C2}
+            ");
+            break;
+        case 2:
+            Console.WriteLine(@"Retirar
+            - Cuanto desas retirar?
+            ");
+            break;
+        case 3:
+            Console.WriteLine(@"Deposito
+            - Cuanto deseas depositar?
+            ");
+            break;
+        case 4:
+            Console.WriteLine("Salida del sistema");
+            break;
+    }
+}
+
+
