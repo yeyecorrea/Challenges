5a01d44c32da5babb62a28dca92762d783d8d9d9
yeyecorrea <yeyecorrea838@gmail.com>
2022-04-17 17:06:39 -0500

    the height of the user is finished

---
+++ ATM/UserHeight/Program.cs
@@ -0,0 +1,21 @@
+﻿int heigth;
+Console.WriteLine("Ingresar tu estatura: ");
+heigth = Convert.ToInt32(Console.ReadLine());
+
+if (heigth <= 150)
+{
+    Console.WriteLine("Eres un eneano");
+}
+else if (heigth <= 170)
+{
+    Console.WriteLine("Eres promedio");
+}
+else if (heigth <= 200)
+{
+    Console.WriteLine("Eres alto");
+}
+else
+{
+    Console.WriteLine("Eres un gigante");
+}
+
