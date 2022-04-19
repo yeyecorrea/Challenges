b8285faeb0a83bda498ae1c3b8a5a646f16ae811
yeyecorrea <yeyecorrea838@gmail.com>
2022-04-18 21:17:01 -0500

    odd or even project completion

---
+++ ATM/EvenOrOdd/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Ingresa un numero: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0)
+{
+    Console.Clear();
+    Console.WriteLine($"{num} es par");
+}
+else
+{
+    Console.Clear();
+    Console.WriteLine($"{num} no es par");
+}
